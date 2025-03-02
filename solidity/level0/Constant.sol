e915525912fb1a944a36204cf9677390d9382866
akxandy <akxandy@qq.com>
2025-03-02 17:47:16 +0800

    常量学习

---
+++ solidity/level0/Constant.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    // 常量变量的编码约定是使用大写字母
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
